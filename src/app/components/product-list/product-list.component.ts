b98017e6383943624d39171eca4b2267d2df883b
KevimLSDS <kevim.lhouis@outlook.com>
2022-08-17 22:16:57 -0300

    Adding a list of products

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,15 +1,29 @@
+import { ProductService } from './../../services/product.service';
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
+  products: Product[] = [];
 
-  constructor() { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    this.listProducts();
   }
 
+  listProducts() {
+    this.productService.getProductList().subscribe({
+      next: (data: Product[]) => {
+        this.products = data;
+      },
+      error: (e: any) => {
+        console.log(e);
+      },
+    });
+  }
 }
