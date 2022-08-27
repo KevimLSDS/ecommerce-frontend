3444a3a148debdda95f9002b2429f61a3e17de8a
KevimLSDS <kevim.lhouis@outlook.com>
2022-08-26 21:02:24 -0300

    Setting page size

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -97,4 +97,10 @@ export class ProductListComponent implements OnInit {
       },
     });
   }
+
+  updatePageSize(pageSize: string): void {
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
+  }
 }
