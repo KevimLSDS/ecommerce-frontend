2a43fb666ac2559d15926ef21f4c12c42ca0927f
KevimLSDS <kevim.lhouis@outlook.com>
2022-09-13 21:34:42 -0300

    Adding Okta Auth to the app and config the login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProductDetailsComponent } from './components/product-details/product-de
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: 'checkout', component: CheckoutComponent },
@@ -37,6 +38,7 @@ const routes: Routes = [
     CartStatusComponent,
     CartDetailsComponent,
     CheckoutComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oa6hqt6cpuL4SzLi5d7',
+        issuer: 'https://dev-01977430.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email'],
+    },
+};
