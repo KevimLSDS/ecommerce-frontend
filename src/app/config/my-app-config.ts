7e6b90a07775493c772d6c54006d9442e1827cd7
KevimLSDS <kevim.lhouis@outlook.com>
2022-09-22 16:11:51 -0300

    Securing frontend with HTTPS

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
     oidc: {
         clientId: '0oa6hqt6cpuL4SzLi5d7',
         issuer: 'https://dev-01977430.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email'],
     },
 };
